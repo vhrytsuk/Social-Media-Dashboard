87a221d13183774e49b8f94234a721f701c9042b
Hrytsuk Valentyn <vhrytsuk@Hrytsuks-MacBook-Pro.local>
2024-09-30 03:31:30 +0300

    Generate finale build

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,33 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
-import tsconfigPaths from "vite-tsconfig-paths";
+import react from '@vitejs/plugin-react-swc';
+import { defineConfig } from 'vite';
+import tsconfigPaths from 'vite-tsconfig-paths';
 
 export default defineConfig({
-  plugins: [tsconfigPaths(), react()]
+  plugins: [tsconfigPaths(), react()],
+
+  build: {
+    target: 'esnext',
+    minify: 'terser',
+    sourcemap: process.env.NODE_ENV !== 'production',
+    rollupOptions: {
+      output: {
+        manualChunks(id) {
+          if (id.includes('node_modules')) {
+            return 'vendor';
+          }
+        },
+      },
+    },
+    terserOptions: {
+      compress: {
+        drop_console: true, 
+        drop_debugger: true, 
+      },
+    },
+    cssCodeSplit: true,
+  },
+
+  optimizeDeps: {
+    include: ['react', 'react-dom', 'react-router-dom'],
+  },
 });
